d0f528e0627f6c52102558da5b2333b19b842774
kevinnadar <jesikamaraj@gmail.com>
2022-07-08 22:20:11 +0530

    bug fixes

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ class Bot(Client):
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
             workers=50,
-            plugins={"root": "plugins"},
+            plugins = dict(root="plugins"),
             sleep_threshold=5,
         )
 
